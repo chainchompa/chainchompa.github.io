7cd3fd7eaa776452376903173ae9cded9872c218
chainchompa <chainchompa@gmail.com>
2019-01-14 23:52:16 -0500

    created about page

---
+++ portfolio-website/src/components/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about">
+        <p>I'm a full stack developer that loves building web apps and learning new ways to solve problems.</p>
+        <p>Skills:</p>
+        <ul>
+            <li>HTML & CSS</li>
+            <li>LESS</li>
+            <li>React</li>
+            <li>Redux</li>
+            <li>Node.js</li>
+            <li>Express</li>
+            <li>Python</li>
+            <li>Django</li>
+            
+        </ul>
+    </div>
+  );
+};
+
+export default About;
