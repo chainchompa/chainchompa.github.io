cebef8b82ae97abda14c0a081082578e085eb5bc
chainchompa <chainchompa@gmail.com>
2019-01-26 19:41:19 -0500

    set up routes

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -3,13 +3,19 @@ import "./App.css";
 import { Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 class App extends Component {
   render() {
     return (
       <div className="App-Container">
         <Navigation />
-        <About />
+        <Route exact path="/" component={About} />
+        <Route exact path="/skills" component={Skills} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
       </div>
     );
   }
---
+++ portfolio-website/src/components/Projects.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div className="projects">
+      <div className="project-container">
+        <h3>Game of Life</h3>
+        <a href="http://jenniferplayer.me/Game-Of-Life/">View Website</a>
+        <a href="https://github.com/chainchompa/Conways-Life">Source Code</a>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
