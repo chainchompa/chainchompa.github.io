8cfd66c71cc59ef5bd5c45d8210f8da809e78b57
chainchompa <chainchompa@gmail.com>
2019-01-29 01:45:26 -0500

    Updates

---
+++ precache-manifest.11f70809847921a21022781856c424fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d941232120af51013cc42aa73194c6",
+    "url": "/portfolio-website/static/media/notes.f2d94123.png"
+  },
+  {
+    "revision": "af643d927a6743a233d2e001e66ae6a8",
+    "url": "/portfolio-website/static/media/gameoflife.af643d92.png"
+  },
+  {
+    "revision": "ad8d3ebf4b4fa643a47d352eff601b64",
+    "url": "/portfolio-website/static/media/architects.ad8d3ebf.png"
+  },
+  {
+    "revision": "2b256896c0c48cf1d86c",
+    "url": "/portfolio-website/static/js/runtime~main.2b256896.js"
+  },
+  {
+    "revision": "50df34eff0a093abdc2f",
+    "url": "/portfolio-website/static/js/main.50df34ef.chunk.js"
+  },
+  {
+    "revision": "76c4fe721e42a2e3b6f9",
+    "url": "/portfolio-website/static/js/1.76c4fe72.chunk.js"
+  },
+  {
+    "revision": "50df34eff0a093abdc2f",
+    "url": "/portfolio-website/static/css/main.d7aa5778.chunk.css"
+  },
+  {
+    "revision": "4d95764dba740c4a6cc3cb3154c73bf4",
+    "url": "/portfolio-website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c5d3afe4a1e4fcac86c9b11a89b9d62.js"
+  "/portfolio-website/precache-manifest.11f70809847921a21022781856c424fd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio-website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
