de3ad603548c00e5dcd19052a7beabcb5ea9d4f6
chainchompa <chainchompa@gmail.com>
2020-06-29 15:27:26 -0400

    Updates

---
+++ precache-manifest.7b1601e03ffdd98674e2cff2ee9ac862.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1a21a05ffed816723a6",
+    "url": "/static/css/main.422d1c02.chunk.css"
+  },
+  {
+    "revision": "e1a21a05ffed816723a6",
+    "url": "/static/js/main.e1a21a05.chunk.js"
+  },
+  {
+    "revision": "f7ed0939022ae75f566d",
+    "url": "/static/js/1.f7ed0939.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bfb1350b248ddb871ff0dc2cc40ddd59",
+    "url": "/static/media/JenniferResume.bfb1350b.pdf"
+  },
+  {
+    "revision": "af643d927a6743a233d2e001e66ae6a8",
+    "url": "/static/media/gameoflife.af643d92.png"
+  },
+  {
+    "revision": "537bd24966d45b1e0ac5e2061d0ce920",
+    "url": "/static/media/chattr.537bd249.png"
+  },
+  {
+    "revision": "ad8d3ebf4b4fa643a47d352eff601b64",
+    "url": "/static/media/architects.ad8d3ebf.png"
+  },
+  {
+    "revision": "7f6f3e557ac6612ecfceb8c1f28f7927",
+    "url": "/index.html"
+  }
+];
